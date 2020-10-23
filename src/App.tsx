5bc05a08aae33cb43105722774294d10576b2dc1
ttzztztz <ttzztztz@outlook.com>
2020-10-23 10:42:22 +0800

    feat: navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,26 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+
 import { blue, orange } from "@material-ui/core/colors";
+
 import "./App.css";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: blue['A200'],
+      main: blue["A200"],
     },
     secondary: {
       main: orange["A200"],
-    }
+    },
   },
 });
 
@@ -34,10 +43,75 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  list: {
+    width: 250,
+  },
 }));
 
 const App = () => {
   const classes = useStyles();
+  const [navigationOpenState, setNavigationOpenState] = React.useState(false);
+
+  const toggleDrawer = (open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent
+  ) => {
+    console.log(event);
+    if (
+      event.type === "keydown" &&
+      ((event as React.KeyboardEvent).key === "Tab" ||
+        (event as React.KeyboardEvent).key === "Shift")
+    ) {
+      return;
+    }
+
+    setNavigationOpenState(open);
+  };
+  const NavigationList = () => {
+    return (
+      <div
+        className={classes.list}
+        role="presentation"
+        onClick={toggleDrawer(false)}
+        onKeyDown={toggleDrawer(false)}
+      >
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Problems" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Contest" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Community" />
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Login" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Register" />
+          </ListItem>
+        </List>
+      </div>
+    );
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -46,11 +120,19 @@ const App = () => {
           <IconButton
             edge="start"
             className={classes.menuButton}
+            onClick={toggleDrawer(true)}
             color="inherit"
             aria-label="menu"
           >
             <MenuIcon />
           </IconButton>
+          <Drawer
+            anchor="left"
+            open={navigationOpenState}
+            onClose={toggleDrawer(false)}
+          >
+            <NavigationList />
+          </Drawer>
           <Typography variant="h6" className={classes.title}>
             Rabbit OJ v2
           </Typography>
@@ -59,6 +141,6 @@ const App = () => {
       </AppBar>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
